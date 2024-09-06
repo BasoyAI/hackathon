97705f6a8591c29058cd3d131252756c2f4203d6
Zahid Esad <116666407+zahidesad@users.noreply.github.com>
2024-09-06 04:36:05 +0300

    Back Button edited

--- hackathon/lib/pages/analyse_page.dart
+++ hackathon/lib/pages/analyse_page.dart
@@ -4,82 +4,85 @@ class AnalysePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        width: 400,
-        height: 844,
-        color: Color(0xFF14161B),
-        child: Column(
-          mainAxisSize: MainAxisSize.max, // Allow it to take up full height
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              alignment: Alignment.centerLeft,
-              width: 30,
-              height: 30,
-              margin: EdgeInsets.all(10),
-              decoration: ShapeDecoration(
-                color:
-                Color(0xFFDDD9C8), // Background color for the button
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                // Makes the background round
-              ),
-              child: IconButton(
-                icon: Icon(Icons.arrow_back, size: 15, color: Color(0xFF14161B)),
-                onPressed: () {
-                  // Add your forward button logic here
-                },
-              ),
-            ),
-            Expanded( // Ensure the column takes up available space
-              child: SingleChildScrollView( // Allow scrolling for long content
-                child: Column(
-                  children: [
-                    _buildAnalizContainer('ANALİZ 1 -  The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere.'),
-                    SizedBox(height: 8),
-                    _buildAnalizContainer('ANALİZ 2 -  The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere.'),
-                    SizedBox(height: 8),
-                    _buildAnalizContainer('ANALİZ 3 -  The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere.'),
-                    SizedBox(height: 8),
-                    _buildAnalizContainer('ANALİZ 4 -  The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere.'),
-                    SizedBox(height: 8),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              height: 88,
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 360,
-                    height: 56,
-                    decoration: ShapeDecoration(
-                      color: Color(0xFFDFD8C8),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(28),
-                      ),
+      child: Scaffold(
+        body: Container(
+          width: 400,
+          height: 844,
+          color: Color(0xFF14161B),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                alignment: FractionalOffset.bottomLeft,
+                height: 40, // Sabit yükseklik
+                color: Color(0xFF14161B), // Arkaplan rengi
+                child: Container(
+                  decoration: ShapeDecoration(
+                    color: Color(0xFFDDD9C8), // Butonun arka plan rengi
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    child: Center(
-                      child: Text(
-                        'Konuşma başlatmak için tıklayınız',
-                        style: TextStyle(
-                          color: Colors.black.withOpacity(0.46),
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back, color: Color(0xFF14161B)),
+                    onPressed: () {
+                      // Geri tuşu işlemleri burada olacak
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      _buildAnalizContainer('ANALİZ 1 - The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere.'),
+                      SizedBox(height: 8),
+                      _buildAnalizContainer('ANALİZ 2 - The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere.'),
+                      SizedBox(height: 8),
+                      _buildAnalizContainer('ANALİZ 3 - The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere.'),
+                      SizedBox(height: 8),
+                      _buildAnalizContainer('ANALİZ 4 - The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere.'),
+                      SizedBox(height: 8),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 88,
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 360,
+                      height: 56,
+                      decoration: ShapeDecoration(
+                        color: Color(0xFFDFD8C8),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(28),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Konuşma başlatmak için tıklayınız',
+                          style: TextStyle(
+                            color: Colors.black.withOpacity(0.46),
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
